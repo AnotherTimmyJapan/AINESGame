563510ed58ad00fa567facfede9578cfa6bdeb3a
AnotherTimmyJapan <timmytyanpuipuisub2@gmail.com>
2026-03-10 09:50:53 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,4 @@
-#define PPU_MASK    *((unsigned char*)0x2001)
-#define PPU_ADDR    *((unsigned char*)0x2006)
-#define PPU_DATA    *((unsigned char*)0x2007)
+#include "nes_hw.h"
 
 // Explicitly tell C that this function is in our ASM file
 extern void __fastcall__ wait_vblank(void);
@@ -8,10 +6,12 @@ extern void __fastcall__ wait_vblank(void);
 void main (void) {
     wait_vblank();
     
-    PPU_ADDR = 0x3F; PPU_ADDR = 0x00;
-    PPU_DATA = 0x11; // Blue
+    // Using the clean names from our header
+    PPU_ADDR = 0x3F; 
+    PPU_ADDR = 0x00;
+    PPU_DATA = 0x11; // Dark Blue
 
-    PPU_MASK = 0x1E; 
+    PPU_MASK = 0x1E; // Show background and sprites
 
     while (1) {
         wait_vblank();
