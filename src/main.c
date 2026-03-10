07c4eea7b533893f9b573a0a1592ef2f8afe5a58
AnotherTimmyJapan <timmytyanpuipuisub2@gmail.com>
2026-03-10 09:47:08 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,20 +2,18 @@
 #define PPU_ADDR    *((unsigned char*)0x2006)
 #define PPU_DATA    *((unsigned char*)0x2007)
 
-// We call the assembly version of wait_vblank now for better stability
-extern void wait_vblank(void);
+// Explicitly tell C that this function is in our ASM file
+extern void __fastcall__ wait_vblank(void);
 
 void main (void) {
-    // 1. Initialize Palette
     wait_vblank();
+    
     PPU_ADDR = 0x3F; PPU_ADDR = 0x00;
-    PPU_DATA = 0x11; // Blue background
+    PPU_DATA = 0x11; // Blue
 
-    // 2. Enable Graphics
     PPU_MASK = 0x1E; 
 
     while (1) {
         wait_vblank();
-        // Just keep the lights on
     }
 }
