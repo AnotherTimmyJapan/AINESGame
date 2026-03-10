45a63d231f394a21d45f1b899237027253edf2d0
AnotherTimmyJapan <timmytyanpuipuisub2@gmail.com>
2026-03-10 09:17:40 +0900

    Update crt0.s

--- src/crt0.s
+++ src/crt0.s
@@ -1,22 +1,25 @@
-.export _main
+.import _main
 .export __STARTUP__ : abs = 1
+.import __STACKSIZE__ ; Linker generated
 
 .segment "HEADER"
-    .byte $4e, $45, $53, $1a ; "NES" + EOF
-    .byte $02                ; 2x 16KB PRG
-    .byte $01                ; 1x 8KB CHR
-    .byte $00, $00           ; Mapper 0, NROM
-    .res 8, $00              ; Padding
+    .byte $4e, $45, $53, $1a
+    .byte $02 ; 32k PRG
+    .byte $01 ; 8k CHR
+    .byte $00, $00
+
+.segment "STARTUP"
+reset:
+    sei
+    cld
+    ldx #$ff
+    txs
+    jmp _main
+
+.segment "VECTORS"
+    .word 0 ; NMI
+    .word reset
+    .word 0 ; IRQ
 
 .segment "CHARS"
-    ; A simple 8x8 square tile
-    .byte %00111100
-    .byte %01111110
-    .byte %11111111
-    .byte %11111111
-    .byte %11111111
-    .byte %11111111
-    .byte %01111110
-    .byte %00111100
-    .res 8184, $00 ; Fill the rest of the 8KB with zeros
-
+    .res 8192, $AA ; Fill with a pattern so we can see if it's working
