961550f5a5c0bec263b8c5c29a9b132db3ba9245
AnotherTimmyJapan <timmytyanpuipuisub2@gmail.com>
2026-03-10 09:37:55 +0900

    Update crt0.s

--- src/crt0.s
+++ src/crt0.s
@@ -1,6 +1,6 @@
 .import _main
 .export __STARTUP__ : abs = 1
-.import __STACKSIZE__ ; Linker generated
+.export _wait_vblank
 
 .segment "HEADER"
     .byte $4e, $45, $53, $1a
@@ -8,18 +8,38 @@
     .byte $01 ; 8k CHR
     .byte $00, $00
 
-.segment "STARTUP"
+.segment "CODE"
+_wait_vblank:
+    bit $2002
+:   bit $2002
+    bpl :-
+    rts
+
 reset:
     sei
     cld
+    ldx #$00
+    stx $2000
+    stx $2001
+    
+    ; Wait for PPU to stabilize
+    jsr _wait_vblank
+    jsr _wait_vblank
+
     ldx #$ff
     txs
     jmp _main
 
+nmi:
+    rti ; Return from interrupt (does nothing for now)
+
+irq:
+    rti
+
 .segment "VECTORS"
-    .word 0 ; NMI
-    .word reset
-    .word 0 ; IRQ
+    .word nmi    ; $FFFA (NMI)
+    .word reset  ; $FFFC (Reset)
+    .word irq    ; $FFFE (IRQ)
 
 .segment "CHARS"
-    .res 8192, $AA ; Fill with a pattern so we can see if it's working
+    .res 8192, $AA ; Fill with pattern
