798cbf24692309e6be965f31001806299ae3b68c
AnotherTimmyJapan <timmytyanpuipuisub2@gmail.com>
2026-03-10 09:15:25 +0900

    Update crt0.s

--- src/crt0.s
+++ src/crt0.s
@@ -1,3 +1,13 @@
+.export _main
+.export __STARTUP__ : abs = 1
+
+.segment "HEADER"
+    .byte $4e, $45, $53, $1a ; "NES" + EOF
+    .byte $02                ; 2x 16KB PRG
+    .byte $01                ; 1x 8KB CHR
+    .byte $00, $00           ; Mapper 0, NROM
+    .res 8, $00              ; Padding
+
 .segment "CHARS"
     ; A simple 8x8 square tile
     .byte %00111100
